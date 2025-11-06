ec5de1fd34a7047ce1d44d926c4a30f8c7ce89dc
Justin Caveda <justincaveda11031@gmail.com>
2025-11-06 17:52:01 -0600

    Fix compose setup copy path (#30)

--- files/system/usr/share/setup-scripts/compose.sh
+++ files/system/usr/share/setup-scripts/compose.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-mkdir /home/core/setup
-cp -r /usr/share/setup-scripts/compose-setup /home/core/setup
+mkdir -p /home/core/setup
+cp -r /usr/share/compose-setup /home/core/setup
 chown -R core:core /home/core/setup
